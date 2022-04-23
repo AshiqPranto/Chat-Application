5c0894d2f89733ce1b3b770a7bac8a515c2eab28
Ashiq Uddin Pranto <ashiqpranto@gmail.com>
2022-04-23 15:29:17 +0600

    /users endpoint done lesson-3

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,14 +1,37 @@
 //external imports
 const express = require('express');
 
-//internal imports
-const {getUser} = require('../controller/usersController');
-const decorateHtmlResponse = require('../middlewears/common/decorateHtmlResponse');
+// internal imports
+const {
+    getUsers,
+    addUser,
+    removeUser,
+  } = require("../controller/usersController");
+  const decorateHtmlResponse = require("../middlewears/common/decorateHtmlResponse");
+  const avatarUpload = require("../middlewears/users/avatarUpload");
+  const {
+    addUserValidators,
+    addUserValidationHandler,
+  } = require("../middlewears/users/userValidators");
 
 const router = express.Router();
 
-//login page
-router.get("/",decorateHtmlResponse("Users"),getUser);
+//Users page
+router.get("/",decorateHtmlResponse("Users"),getUsers);
+
+//add user
+router.post(
+    "/",
+    avatarUpload,
+    addUserValidators,
+    addUserValidationHandler,
+    addUser
+  );
+  
+  // remove user
+  router.delete("/:id", removeUser);
+  
+
 
 module.exports = router;
 
